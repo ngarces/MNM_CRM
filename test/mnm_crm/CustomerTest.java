54f8307446099e85d020ae8667a21423583d1c30
ngarces <37097880+ngarces@users.noreply.github.com>
2019-03-10 17:06:26 -0700

    Added JUnit test for Customer class

---
+++ test/mnm_crm/CustomerTest.java
@@ -0,0 +1,48 @@
+package mnm_crm;
+
+import mnm_crm.Customer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ngarc
+ */
+public class CustomerTest {
+    Customer cust1;
+    
+    @Before
+    public void initialize() {
+        cust1 = new Customer("Doe", "John", "someEmail@somewebsite.com", "(123)456-7890", "123 Some Street");
+        cust1.printCustomer();
+    }
+    
+    @Test
+    public void testLastName() {
+        assertEquals("Doe", cust1.getLastName());
+    }
+    
+    @Test
+    public void testFirstName() {
+        assertEquals("John", cust1.getFirstName());
+    }
+    
+    @Test
+    public void testEmail() {
+        assertEquals("someEmail@somewebsite.com", cust1.getEmail());
+    }
+    
+    @Test
+    public void testPhoneNum() {
+        assertEquals("(123)456-7890", cust1.getPhoneNum());
+    }
+    
+    @Test
+    public void testAddress() {
+        assertEquals("123 Some Street", cust1.getAddress());
+    }
+}
