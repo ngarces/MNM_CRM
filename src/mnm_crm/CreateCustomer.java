6c14a0b53d358b124135418463c05c4c64421e00
Fudgenuggets <37097880+Fudgenuggets@users.noreply.github.com>
2019-03-13 14:17:50 -0700

    Merged and fixed some issues with Megnha's files

--- src/mnm_crm/CreateCustomer.java
+++ src/mnm_crm/CreateCustomer.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package salesforceproj;
+package mnm_crm;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/mnm_crm/SalesforceProj.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mnm_crm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author meghnasawhney
+ */
+public class SalesforceProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu m =new MainMenu();
+        m.chooseOption();
+       
+    
+}
+}
